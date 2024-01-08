8b62ce0d6d4b99e5278dfe0a654c38242571aa9e
Emre Bekfilavi <101346325+EmreBekfilavi@users.noreply.github.com>
2024-01-08 18:48:48 +0300

    3. Odev Yuklemesi

---
+++ Odev3/EmreBekfilaviogluOdev/EmreBekfilaviogluOdev/Program.cs
@@ -0,0 +1,22 @@
+﻿int sayi, toplam;
+
+toplam = 0;
+
+Console.Write("Lütfen Kaça Kadar İşlem Yapmak İstediğinizi Belirtiniz: ");
+sayi = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= sayi; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine("Çıkar");
+        toplam = toplam - i;
+    }
+    else
+    {
+        Console.WriteLine("Topla");
+        toplam += i;
+    }
+}
+
+Console.WriteLine($"İstediğiniz İşlemin Sonucu => {toplam}");
