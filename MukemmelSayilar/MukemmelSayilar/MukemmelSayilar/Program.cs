990705b34d7923de3460bb44202487a1b7b54372
Emre Bekfilavi <101346325+EmreBekfilavi@users.noreply.github.com>
2024-01-08 19:14:00 +0300

    Mukemmel Sayilar Uygulamasi Yapildi

---
+++ MukemmelSayilar/MukemmelSayilar/MukemmelSayilar/Program.cs
@@ -0,0 +1,31 @@
+﻿int sayi;
+string durum;
+int toplam;
+
+
+do
+{
+    Console.Write("Mükemmelliğini Merak Ettiğiniz Sayıyı Giriniz: ");
+    sayi = int.Parse(Console.ReadLine());
+
+    toplam = 0;
+
+    for(int i = 1;  i < sayi; i++)
+    {
+        if (sayi % i == 0)
+        {
+            toplam = toplam + i;
+        }
+    }
+
+    if (toplam == sayi)
+        Console.WriteLine("Girdiğinizi Sayı Mükemmel Sayıdır");
+    else
+        Console.WriteLine("Girdiğiniz Sayı Mükemmel Değildir");
+
+    Console.Write("Yeni Bir Sayı Girmek İstiyor Musunuz? => ");
+    durum = Console.ReadLine();
+
+} while (durum == "Evet");
+
+
