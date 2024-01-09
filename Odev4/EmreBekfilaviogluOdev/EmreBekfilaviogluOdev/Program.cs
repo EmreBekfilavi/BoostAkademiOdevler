8e0764a517d664e175701d482e93812c5c92ee06
Emre Bekfilavi <101346325+EmreBekfilavi@users.noreply.github.com>
2024-01-09 13:42:54 +0300

    Asal Carpanlar Odevi Yapildi

---
+++ Odev4/EmreBekfilaviogluOdev/EmreBekfilaviogluOdev/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] asalCarpanlar = new int[10];
+int sayi;
+bool kontrol = true;
+int index = 0;
+
+Console.Write("Asal Çarpanlarını Merak Ettiğiniz Sayıyı Giriniz: ");
+sayi = int.Parse(Console.ReadLine());
+
+for (int i = 2;  i < sayi; i++)
+{
+    if(sayi % i == 0)
+    {
+        for(int j = 2; j<i; j++)
+        {
+            if(i % j == 0)
+            {
+                kontrol = false;
+                break;
+            }
+
+            kontrol = true;
+
+        }
+
+        if (kontrol)
+        {
+            asalCarpanlar[index] = i;
+            index++;
+        }
+    }
+}
+
+Console.WriteLine("Asal Çarpanlar\n------------");
+
+foreach (int asal in asalCarpanlar)
+{
+    Console.WriteLine(asal);
+}
